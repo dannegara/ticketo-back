f270f799013e4648901933dcebba460ae61b9f5f
Dan Negara <dannegareh@gmail.com>
2020-02-18 01:03:07 +0200

    bcrypt for login and register added

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const myPlaintextPassword = '12345678';
 
 const router = express.Router();
 
 router.get('/login', (req, res) => {
     res.json({msg: 'Logged'});
+});
+
+router.post('/register', (req, res) => {
+    bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
+        // Store hash in your password DB.
+        console.log(hash);
+    });
+});
+
+router.post('/login', (req, res) => {
+    bcrypt.compare(myPlaintextPassword, '$2b$10$AzdezRGv8h.n6/NdfLWIOOteMV6GuPTq6voNiCrlS1o380JuHD4im', function(err, result) {
+        // result == true
+        console.log(result);
+    });
 });
 
 module.exports = router;
