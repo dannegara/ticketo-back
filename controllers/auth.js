da983e8be985405c05c7f81e2ade8a4a434aa063
Dan Negara <dannegareh@gmail.com>
2020-02-18 10:24:42 +0200

    login and register api demo created

---
+++ controllers/auth.js
@@ -0,0 +1,49 @@
+const bcrypt = require('bcrypt');
+const { db } = require('../config/database');
+const saltRounds = 10;
+
+class Auth{
+    static login = (req, res) => {
+        const { login, password } = req.body;
+
+        db.query("call Login(?)", [login], (databaseError, databaseResponse) => {
+            if(databaseError) res.status(500).json({ err: 'database error' });
+            if(databaseResponse[0].length === 0) res.json({ msg: 'invalid login or password' });
+            else{
+                const databaseHashedPassword = databaseResponse[0][0].password;
+                bcrypt.compare(password,databaseHashedPassword, (err, result) => {
+                    if(err) res.json({ err: 'server error' });
+                    if(result === false){
+                        res.json({ msg: 'invalid login or password' });
+                    }else{
+                        res.json({ msg: 'login success' });
+                    }
+                });
+            }
+        });
+    }
+    static register = (req, res) => {
+        const { 
+            username, 
+            password, 
+            email,
+            firstname,
+            lastname 
+        } = req.body;
+        bcrypt.hash(password, saltRounds, function(err, hashedPassword) {
+            if(err) throw err;
+            db.query("call Register(?,?,?,?,?)", [
+                username,
+                email,
+                firstname,
+                lastname,
+                hashedPassword
+            ], (databaseError, databaseResponse) => {
+                if(databaseError) res.status(500).json({msg: 'Database error'});
+                res.status(200).json(databaseResponse[0][0]);
+            });
+        });
+    }
+}
+
+module.exports = Auth;
--- routes/auth.js
+++ routes/auth.js
@@ -1,26 +1,10 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
-const myPlaintextPassword = '12345678';
+const Auth = require('../controllers/auth');
 
 const router = express.Router();
 
-router.get('/login', (req, res) => {
-    res.json({msg: 'Logged'});
-});
+router.post('/register', Auth.register);
 
-router.post('/register', (req, res) => {
-    bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
-        // Store hash in your password DB.
-        console.log(hash);
-    });
-});
-
-router.post('/login', (req, res) => {
-    bcrypt.compare(myPlaintextPassword, '$2b$10$AzdezRGv8h.n6/NdfLWIOOteMV6GuPTq6voNiCrlS1o380JuHD4im', function(err, result) {
-        // result == true
-        console.log(result);
-    });
-});
+router.post('/login', Auth.login);
 
 module.exports = router;
